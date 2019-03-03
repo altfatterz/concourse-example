c571a4778138c62be07b07caf0c93b1832bb3d27
Zoltan Altfatter <altfatterz@gmail.com>
2019-03-03 11:29:16 +0100

    initial maven cache

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -2,6 +2,13 @@
 
 set -e -u -x
 
+export ROOT_FOLDER=$( pwd )
+
+M2_HOME="${HOME}/.m2"
+M2_CACHE="${ROOT_FOLDER}/maven"
+
+[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
+
 cd source-code
 ./mvnw clean install
 
